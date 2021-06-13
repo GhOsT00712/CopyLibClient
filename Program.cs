efaa834c3e96a51be1064ca9acc4c79474a40200
pidubey <pidubey@adobe.com>
2021-06-13 15:16:43 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using CopyLib;
+namespace CopyDirectory
+{
+    class Program
+    {
+        public static ICore copyUtil;
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Please provide the path of source folder :");
+            String sourcePath = Console.ReadLine();
+            Console.WriteLine("Please provide the path of target folder :");
+            String destinationPath = Console.ReadLine();
+            copyUtil = new Core();
+            string result = await copyUtil.copyDirAsync(sourcePath, destinationPath);
+            Console.WriteLine(result);
+        }
+    }
+}
